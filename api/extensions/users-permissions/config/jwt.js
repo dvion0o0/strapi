717aca0fe51ed5e48054253b9b79cbe602c94aa8
Dv <digvijayghosh@Dv-MacBook-Pro.local>
2021-08-27 12:12:01 +0530

    upload

---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5a11abf1-67b0-4cc9-be3b-cc4fdb833370'
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,35 @@
-import React from "react"
-
+import React, { useState } from "react"
+import axios from "axios"
 export default function Home() {
-  return <div>Hello world!</div>
+  const [file, setFile] = useState("")
+  const [url, setUrl] = useState(null)
+  const handleChange = async e => {
+    setFile(e.target.files[0])
+  }
+
+  const handleClick = async () => {
+    const form = new FormData()
+    form.append("files", file)
+    const result = await axios.post("http://localhost:1337/upload", form)
+    console.log(result.data[0].url)
+    setUrl(result.data[0].url)
+
+    const post = await axios.post("http://localhost:1337/products", {
+      Name: "Testing",
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, labore.",
+      image: result.data[0].url,
+      images: result.data[0].id,
+    })
+    console.log(post)
+  }
+
+  return (
+    <>
+      {" "}
+      <div>Hello world!2</div>
+      <input type="file" onChange={e => handleChange(e)} />
+      <button onClick={handleClick}>submit</button>
+    </>
+  )
 }
